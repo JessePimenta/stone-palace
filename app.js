4b67091cc9549137ec21f42a34adb7a3bf525c70
Jesse Pimenta <drrrreams@gmail.com>
2015-12-27 19:33:13 -0700

    added animations to words and castle

--- app.js
+++ app.js
@@ -1,1 +1,35 @@
+$(document).ready(function () {
 
+var animationName = 'animated pulse';
+var animationFlash = 'animated flash';
+var secondAnimation = 'animated jello'
+var animationend = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend';
+
+      $('.castle').on('click',function() {
+    $( this ).addClass(secondAnimation).one(animationend,function(){
+    $( this ).removeClass(secondAnimation);
+      });
+    });
+
+
+    $('.exhibition').on('mouseleave',function(){
+      $(this).addClass(animationName).one(animationend,function(){
+      $(this).removeClass(animationName)
+    });
+    })
+    $('.max').hover(function(){
+      $(this).addClass(animationName).one(animationend,function(){
+      $(this).removeClass(animationName)
+    });
+    })
+    $('.datesNTime').hover(function(){
+      $(this).addClass(animationName).one(animationend,function(){
+      $(this).removeClass(animationName)
+    });
+    })
+    $('.lildude').hover(function(){
+      $(this).addClass(secondAnimation).one(animationend,function(){
+      $(this).removeClass(secondAnimation)
+    });
+    })
+	});
